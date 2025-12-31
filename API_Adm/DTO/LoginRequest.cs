c3fe3bbe1fada27a418ffd9c361d81f86dccde72
datvu298204 <teddyleorank@gmail.com>
2025-12-31 16:57:28 +0700

    change

---
+++ API_Adm/DTO/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Adm.DTO
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
