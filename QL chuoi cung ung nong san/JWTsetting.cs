5169ec3244f42ccd85d249480558566801439739
datvu298204 <teddyleorank@gmail.com>
2025-12-31 16:01:09 +0700

    jwt set

---
+++ QL chuoi cung ung nong san/JWTsetting.cs
@@ -0,0 +1,10 @@
+﻿namespace QL_chuoi_cung_ung_nong_san
+{
+    public class JWTsetting
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpireMinutes { get; set; }
+    }
+}
